885bd2ae4928c0b28daab0b6ee8b016ac611248f
Rafael Schreiber <raf.schreiber@gmail.com>
2018-06-07 16:47:55 +0200

    Added Nickname Support, Improved Stability Special Chat commands can be executed with %[command]

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+
+server_address = ("127.0.0.1", 8080)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create socket
+client_socket.connect(server_address) # specify address
+
+print("Type in your username:")
+username = str(input(">>> "))
+client_socket.send(bytes(username, "utf8"))
+print("Welcome " + username)
+
+while True:
+    try:
+        msg = str(input(">>> "))
+        if msg == "%exit":
+            client_socket.send(bytes("%exit", "utf8"))
+            print("Connection closed")
+            exit(0)
+        client_socket.send(bytes(msg, "utf8"))
+    except KeyboardInterrupt:
+        client_socket.send(bytes("%exit", "utf8"))
+        print("Connection closed")
+        exit(0)
---
+++ server.py
@@ -0,0 +1,36 @@
+import threading
+import socket
+
+class connectedHost(threading.Thread):
+    """Class for the connection Treads"""
+    def __init__(self, connection, address, id):
+        self.connection = connection
+        self.ip, self.port = address
+        self.id = id
+        self.nickname = str(self.connection.recv(2048), "utf8")
+        print(self.nickname + " is online on " + self.ip + ":" + str(self.port) + " with PID " + str(self.id))
+        threading.Thread.__init__(self)
+
+
+    def run(self):
+        while True:
+            msg = self.connection.recv(2048)
+            msg = str(msg, "utf8")
+            if msg == "%exit":
+                print(self.nickname + " on " + self.ip + ":" + str(self.port) + " with PID " + str(self.id) + " left")
+                return True #
+            print(self.nickname + ": " + msg)
+
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("127.0.0.1", 8080))
+server_socket.listen(10)
+connectionDictionary = {}
+connectionCounter = 0
+while True:
+    (client_socket, addr) = server_socket.accept()
+    connectionDictionary["conn" + str(connectionCounter)] = connectedHost(client_socket, addr, connectionCounter)
+    connectionDictionary["conn" + str(connectionCounter)].start()
+    connectionCounter += 1
+    #print(connectionDictionary)
