ac7a3b30b49d4fe45f5c73d0ee5475f8213ec2da
Simon <>
2018-06-14 10:27:53 +0200

    Client added

--- Client/src/connect.js
+++ Client/src/connect.js
@@ -1,9 +1,21 @@
-var net = require('net');
+let net = require('net');
 
-var client = new net.Socket();
+let client = new net.Socket();
+let $userFormArea = $('#userFormArea');
+let $user = $('#username');
+let $userArea = $('#userArea');
+let $messageArea = $('#messageArea');
+let $messageFromArea = $('#messageFormArea');
 
-client.connect(2018, "2bhif.at", function () {
-    alert('You now connected to 2bhif.at');
-    console.log('Lerry');
-    client.write('Hello!');
+
+client.connect(2018, "localhost");
+
+
+$userFormArea.submit(function (event) {
+    $userArea.hide();
+    $messageArea.show();
+    event.preventDefault();
+    username = '%setusername ' + $user.val();
+    client.write(username);
+    $user.val('');
 });
