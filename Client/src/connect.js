54ac8e7e3e2ef0149b06a660b2afcbf4f531a6c4
Simon <>
2018-06-14 22:24:18 +0200

    Connection with username added

--- Client/src/connect.js
+++ Client/src/connect.js
@@ -1,21 +1,5 @@
-let net = require('net');
-
-let client = new net.Socket();
-let $userFormArea = $('#userFormArea');
-let $user = $('#username');
-let $userArea = $('#userArea');
-let $messageArea = $('#messageArea');
-let $messageFromArea = $('#messageFormArea');
 
 
-client.connect(2018, "localhost");
 
 
-$userFormArea.submit(function (event) {
-    $userArea.hide();
-    $messageArea.show();
-    event.preventDefault();
-    username = '%setusername ' + $user.val();
-    client.write(username);
-    $user.val('');
-});
+
