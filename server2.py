57e79f4f5337331834dd2cce001a3b728a3cd2b0
Rafael Schreiber <raf.schreiber@gmail.com>
2018-06-24 02:17:57 +0200

    Bugfixes

--- server2.py
+++ server2.py
@@ -277,6 +277,8 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
+        print()
         shutdown([])
     except EOFError:
+        print()
         shutdown([])
